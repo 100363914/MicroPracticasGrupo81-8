b2b213d1ec6373a50dbab99677c130e2041a982e
100363914 <58235215+100363914@users.noreply.github.com>
2021-02-23 13:08:17 +0100

    Funcional falta css

--- practica01/src/app/app.component.ts
+++ practica01/src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'practica01';
+  title = 'Comanda';
+  totalPedido = 0;
+  elementoSeleccionado: any; 
+  carta:Array<any> = [
+      {plato: 'Arroz tres delicias',precio: 3.5},
+      {plato: 'Cerdo agridulce',precio: 5.5},
+      {plato: 'Ternera con bambú y setas',precio: 7}];
+  pedido:Array<any>=[];
+
+
+  public addComanda(){
+    console.log(this.elementoSeleccionado.plato);
+    this.pedido.push(this.elementoSeleccionado);
+    this.totalPedido = this.totalPedido + this.elementoSeleccionado.precio;
+  }
+  public delComanda(linea: any){
+    this.totalPedido = this.totalPedido - this.pedido[linea].precio;
+    this.pedido.splice(linea,1);
+  }
+
 }
